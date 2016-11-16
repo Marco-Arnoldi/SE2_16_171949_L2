acdb53bc707dbe25edbabe889b743a0ef84179c3
Marco Arnoldi <Marco Arnoldi>
2016-11-16 10:39:12 +0100

    Implementata funzione visibile_invisibile

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+var visibile = false;
+
 function reset()
 {
     document.getElementById("item").value = "";
     document.getElementById("n_item").value = "";
+}
+
+
+function visibile_invisibile()
+{
+    if(visibile)
+    {
+        div_input.style.display ='none';
+        visibile=false;
+    }
+    else
+    {
+        div_input.style.display ='block';
+        visibile=true;
+    }
+    reset();
 }
